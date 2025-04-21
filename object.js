35aee0e53a598bb1b8977283bc74b4dfa3282cb3
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-21 19:54:28 +0100

    object done

---
+++ object.js
@@ -0,0 +1,11 @@
+const hena ={
+    name:'Hena Khatun',
+    age:34,
+    friends:['Ali','Akbar','Mike'],
+    isMarried:false,
+    fathersName:'Joe',
+    mothersName:'Donald'
+};
+console.log(Object.keys(hena));
+console.log(Object.values(hena));
+console.log(hena.friends[2]);
