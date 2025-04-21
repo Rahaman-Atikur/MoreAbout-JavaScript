d32c428c3ee18173ac8b554500aa132ba35db4d9
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-21 11:08:21 +0100

    method explored

---
+++ array.js
@@ -0,0 +1,11 @@
+let variousArray = ['Mike','data',{},[],877];
+console.log(typeof variousArray[4]);
+/**
+ * push pop shift unshift
+ */
+//
+// variousArray.push('Bangladesh');
+//variousArray.pop();
+//variousArray.shift();
+variousArray.unshift('data');
+console.log(variousArray);
