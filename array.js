889712826b0d01d4695376e9ef03681216894a5d
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-21 19:12:40 +0100

    splice method

--- array.js
+++ array.js
@@ -4,8 +4,10 @@ console.log(typeof variousArray[4]);
  * push pop shift unshift
  */
 //
-// variousArray.push('Bangladesh');
-//variousArray.pop();
-//variousArray.shift();
+variousArray.push('Bangladesh');
+variousArray.pop();
+variousArray.shift();
 variousArray.unshift('data');
+variousArray.splice(1,2);
+
 console.log(variousArray);
