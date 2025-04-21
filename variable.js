15bdceeddad44fbec688e617ab499ba5069a04bb
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-21 10:45:44 +0100

    variable detailed

--- variable.js
+++ variable.js
@@ -1,6 +1,27 @@
-let number =10;
+let number = 10;
 let string = "Atikur";
 let boolen = false;
-let array = [12,12];
+let array = [12, 12];
 
+
+let friend = {
+    name: 'Sakib',
+    age: 32,
+    profession: ' Teacher'
+};
+
+
+let friends = ['Rakib', 'Bokib', 'Sakib'];
+console.log(typeof friend);
+console.log(typeof friends);
+
+
+let numOne = '100';
+let numTwo = 200;
+let convertedNumber = parseInt(numOne);
+
+let sum = numOne - numTwo; // convert string to numbers 
+// but in case of +,- and / they don't convert 
+
+console.log(sum);
 
